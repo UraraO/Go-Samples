384e5a386b53a2d780ff84ee047f438c81a14b0e
UraraO <Haru_UraraO@outlook.com>
2024-07-09 17:40:56 +0800

    feat: dev bus module dev bus module, event type and event handler……

--- go_basics_exercises/src/main.go
+++ go_basics_exercises/src/main.go
@@ -2,23 +2,49 @@ package main
 
 import (
 	"fmt"
+	bus "go_basics_exercises/src/bus_module"
 	"strings"
+	"time"
 	"unicode/utf8"
 )
 
 func main() {
 	// fmt.Println("hello world")
+
+	// 流程控制
 	// Print53()
+
+	// 字符串处理
 	// fmt.Println(utf8.RuneCountInString("hello, 123爱我国防"))
 	// str := "byDSWEW ggg inxudwmn xy"
 	// fmt.Println(CalcNumOfChar(&str))
 	// fmt.Println(ReverseString("foobar"))
 	// fmt.Println(ReverseStringbyPointer(&str))
 	// fmt.Println(ReplaceCharsInString(str, "abc", 4))
+
+	// 结构体、数组、包
 	// containers.StackTest()
-	MapFuncTest()
+
+	// 函数
+	// MapFuncTest()
+
+	//附加题
+	hdl := bus.NormalHandler{}
+	Bus := bus.InitBusModule()
+	event := bus.InitEvent(bus.EVENT_TYPE_DEBUG, "bus test")
+
+	debug1 := Bus.RegisterEventListener(bus.EVENT_TYPE_DEBUG, hdl)
+	debug2 := Bus.RegisterEventListener(bus.EVENT_TYPE_DEBUG, hdl)
+	info1 := Bus.RegisterEventListener(bus.EVENT_TYPE_INFO, hdl)
+
+	Bus.Publish(event)
+	time.Sleep(time.Second * 3)
+	Bus.RemoveEventListener(debug1)
+	Bus.RemoveEventListener(debug2)
+	Bus.RemoveEventListener(info1)
 }
 
+// 流程控制
 func Print53() {
 	for i := 1; i <= 100; i++ {
 		is5 := (i%5 == 0)
@@ -36,6 +62,7 @@ func Print53() {
 
 }
 
+// 字符串处理
 func CalcNumOfChar(src *string) uint {
 	return uint(utf8.RuneCountInString(*src))
 }
@@ -83,6 +110,7 @@ func ReverseString(src string) string {
 	return sb.String()
 }
 
+// 函数
 func mapFunc(f func(int) int, vec []int) []int {
 	res := make([]int, len(vec))
 	for i, v := range vec {
@@ -99,3 +127,5 @@ func MapFuncTest() {
 	res := mapFunc(double, vec)
 	fmt.Println(res)
 }
+
+// 附加题
